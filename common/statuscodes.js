b59159e7323509ad899ff0c007ba3db85f27fb68
Dmitry <zimokk1@gmail.com>
2016-10-25 12:12:34 +0200

    Added statusCodes

---
+++ common/statuscodes.js
@@ -0,0 +1,10 @@
+'use strict';
+let codes = {};
+
+codes.success = 0;
+codes.badRequest = 400;
+codes.unauthorized = 401;
+codes.forbidden = 403;
+codes.serverError = 500;
+
+module.exports = codes;
